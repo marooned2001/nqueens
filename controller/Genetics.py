28eab6a5ccdf3be1aaa5e1afe66dc892688a3390
marooned2001 <mmohamadrezaa.sadeghii@gmail.com>
2025-05-13 20:08:50 +0330

    fitness

--- controller/Genetics.py
+++ controller/Genetics.py
@@ -4,9 +4,35 @@ project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 from model.board import Board
 
+def fitness(chromosome):
+    score = 0
+    for i in range(len(chromosome)):
+        for j in range(len(chromosome)):
+            if j + chromosome[j] == i + chromosome[i]:
+                continue
+            elif j - chromosome[j] == i - chromosome[i]:
+                continue
+            elif i == j :
+                continue
+            score += 1
+    return score / 2
+
+def cross_over(par1, par2, n):
+    
+
+
+p = 10
+n = 4
 community = []
+fitnesses = []
 
-for _ in range(300):
-    community.append(Board(8))
 
-print(community)
+for _ in range(p):
+    community.append(Board(n))
+
+for i in community:
+    fitnesses.append(fitness(i.grid))
+
+community_fitness = dict(zip(community,fitnesses))
+community_fitness = dict(sorted(community_fitness.items(), key=lambda item: item[1], reverse=True))
+
